dab332b0a1ee0d48469793b2d8acfb576779d413
Vaibhav Bansal <vaibhav.bansal@salesforce.com>
2020-12-15 22:01:31 -0800

    fix index.js

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const app = express()
 const port = 3000
 const paytm_routes = require("./paytm");
 const dotenv = require('dotenv');
+var bodyParser = require('body-parser')
 dotenv.config();
 
 console.log("Paytm MID" + process.env.PAYTM_MID)
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
 app.use('/paytm', paytm_routes);
 
 app.listen(port, () => {
